58465e7954c4e1aa7cc1047e6e572656f342e6a5
lavertis <29688908+Lavertis@users.noreply.github.com>
2021-09-16 22:49:31 +0200

    40. Form tag helpers in asp net core

--- Repositories/MockEmployeeRepository.cs
+++ Repositories/MockEmployeeRepository.cs
@@ -13,9 +13,9 @@ namespace EmployeeManagement.Repositories
         {
             this._employeeList = new List<Employee>()
             {
-                new Employee() {Id = 1, Name = "Mary", Department = "HR", Email = "mary@pragimtech.com"},
-                new Employee() {Id = 2, Name = "John", Department = "IT", Email = "john@pragimtech.com"},
-                new Employee() {Id = 3, Name = "Sam", Department = "IT", Email = "sam@pragimtech.com"}
+                new Employee() {Id = 1, Name = "Mary", Department = Department.HR, Email = "mary@pragimtech.com"},
+                new Employee() {Id = 2, Name = "John", Department = Department.IT, Email = "john@pragimtech.com"},
+                new Employee() {Id = 3, Name = "Sam", Department = Department.IT, Email = "sam@pragimtech.com"}
             };
         }
 
