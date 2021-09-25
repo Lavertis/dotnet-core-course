012eeecc80c2ba3cabe2d07a1ce33e1fa1b54622
lavertis <29688908+Lavertis@users.noreply.github.com>
2021-09-25 16:32:18 +0200

    52. Keeping domain models and database schema in sync in asp net core

--- Migrations/20210925142744_AddPhotoPathColumn.Designer.cs
+++ Migrations/20210925142744_AddPhotoPathColumn.Designer.cs
@@ -3,14 +3,16 @@ using EmployeeManagement.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EmployeeManagement.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210925142744_AddPhotoPathColumn")]
+    partial class AddPhotoPathColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
