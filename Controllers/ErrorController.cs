ea4f589ccf0d019e23fbedec21227f39754e70da
lavertis <29688908+Lavertis@users.noreply.github.com>
2021-09-26 00:07:19 +0200

    62. Logging exceptions in ASP NET Core

--- Controllers/ErrorController.cs
+++ Controllers/ErrorController.cs
@@ -6,11 +6,19 @@ using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.Extensions.Logging;
 
 namespace EmployeeManagement.Controllers
 {
     public class ErrorController : Controller
     {
+        private readonly ILogger<ErrorController> _logger;
+
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            _logger = logger;
+        }
+
         [Route("Error/{statusCode}")]
         public IActionResult HttpStatusCodeHandler(int statusCode)
         {
@@ -21,8 +29,8 @@ namespace EmployeeManagement.Controllers
                 case 404:
                 {
                     ViewBag.ErrorMessage = "Sorry, the resource you requested could not be found";
-                    ViewBag.Path = statusCodeResult.OriginalPath;
-                    ViewBag.QS = statusCodeResult.OriginalQueryString;
+                    _logger.LogWarning($"404 Error occurred. Path = {statusCodeResult.OriginalPath}" + 
+                                       $" and QueryString = {statusCodeResult.OriginalQueryString}");
                     break;
                 }
             }
@@ -35,9 +43,7 @@ namespace EmployeeManagement.Controllers
         public IActionResult Error()
         {
             var exceptionDetails = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
-            ViewBag.ExceptionPath = exceptionDetails.Path;
-            ViewBag.ExceptionMessage = exceptionDetails.Error.Message;
-            ViewBag.Stacktrace = exceptionDetails.Error.StackTrace;
+            _logger.LogError($"The path {exceptionDetails.Path} threw an exception {exceptionDetails.Error}");
 
             return View("Error");
         }
