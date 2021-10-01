5ac888607fa316d9bac8e47b772354c05e634248
lavertis <29688908+Lavertis@users.noreply.github.com>
2021-10-02 00:24:26 +0200

    70. Implementing login functionality in asp net core

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -56,5 +56,30 @@ namespace EmployeeManagement.Controllers
 
             return View(model);
         }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result =
+                    await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                ModelState.AddModelError(string.Empty, "Unsuccessful Login Attempt");
+            }
+
+            return View(model);
+        }
     }
 }
